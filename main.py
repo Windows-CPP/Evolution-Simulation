02a2cedbbdbd1d6ccf35c2c0b0ccfdf354214ef3
Cicero  Fryman <bobbycast66@gmail.com>
2021-02-01 16:49:24 +0000

    Updated main,py, Updated roadmap.txt (V.0.0.1 P-A)

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("Evolution Simulation V.0.0.1 P-A")
 
+
+# This is where the fun begins. Variable setup stuff.
+pop1 = 0 # This is population 1.
+pop2 = 0 # This is population 2.
+
+
+# This is where the fun ends. I have to make math work. I'm failing in math. Great. 
